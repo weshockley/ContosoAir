dfe3ab91971764a1c5d5fbd0cd45d1d1823fa39f
EmilysNest <103007371+weshockley@users.noreply.github.com>
2023-03-08 14:33:07 -0800

    Update airports.service.js

--- src/services/airports.service.js
+++ src/services/airports.service.js
@@ -7,9 +7,10 @@ class AirportsService {
         this._airports = airports;
     }
 
-    getAll(){
-        return this._airports.filter(a => a.code).map(avoidEmptyCity);
-    }
+  getAll(){
+ return this._airports.filter(a => a.code).map(avoidEmptyCity).sort((a,
+b) => (a.city > b.city) ? 1 : -1);
+}
 
     getByCode(code) {
         return avoidEmptyCity(this._airports.find(a => a.code == code));
